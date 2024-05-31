235d7159edcbde485544dae24988c9983cfe1ba0
Deval Panchal <devalp2401@gmail.com>
2024-05-31 10:55:47 -0400

    first commit

---
+++ TaskManagementAPI/Program.cs
@@ -0,0 +1,125 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TaskContext>(opt => opt.UseInMemoryDatabase("TaskList"));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment()) {
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { }
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool Completed { get; set; }
+}
+
+public class TaskContext : DbContext
+{
+    public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
+
+    public DbSet<Task> Tasks { get; set; }
+}
+
+[Route("api/[controller]")]
+[ApiController]
+public class TasksController : ControllerBase
+{
+    private readonly TaskContext _context;
+
+    public TasksController(TaskContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Task>>> GetTasks()
+    {
+        return await _context.Tasks.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Task>> GetTask(int id)
+    {
+        var task = await _context.Tasks.FindAsync(id);
+
+        if (task == null)
+        {
+            return NotFound();
+        }
+
+        return task;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Task>> PostTask(Task task)
+    {
+        _context.Tasks.Add(task);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetTask), new { id = task.Id }, task);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTask(int id, Task task)
+    {
+        if (id != task.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(task).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTask(int id)
+    {
+        var task = await _context.Tasks.FindAsync(id);
+
+        if (task == null)
+        {
+            return NotFound();
+        }
+
+        _context.Tasks.Remove(task);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
